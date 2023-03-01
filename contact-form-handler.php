47510d86d99efe7771e424c2150f0540f4634a68
NojusMat <d00249674@student.dkit.ie>
2023-03-01 16:59:04 +0000

    added schema validation email,radio ect

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -2,7 +2,9 @@
 
 $errors = '';
 $myemail = 'D00249674@student.dkit.ie';// <-----Put your DkIT email address here.
-if(empty($_POST['name'])  ||
+if(
+    empty($_POST['userid'])  ||
+   empty($_POST['name'])  ||
    empty($_POST['email']) ||
    empty($_POST['ecode']) ||
    empty($_POST['phone']) ||
@@ -24,7 +26,7 @@ $headers .= 'From: '.$myemail."\r\n".
     'Reply-To: '.$myemail."\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-
+$userid = $_POST['userid'];
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $ecode = $_POST['ecode'];
@@ -44,19 +46,12 @@ $email_address))
     $errors .= "\n Error: Invalid email address";
 }
 
-// if (!preg_match(
-//     "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-//     $address))
-//     {
-//         $errors .= "\n Error:  address";
-//     }
-
 if( empty($errors))
 {
         $to = $myemail;
         $email_subject = "Contact form submission: $name";
         $email_body = "You have received a new message. ".
-        " Here are the details:\n Name: $name \n Email: $email_address\n ecode:$ecode \n Phone:$phone\n city:$city\n region:$region\ndatePicker: $datePicker\\n Message \n $message";
+        " Here are the details:\nUsername :$userid\n Name: $name \n Email: $email_address\n ecode:$ecode \n Phone:$phone\n city:$city\n region:$region\ndatePicker: $datePicker\\n Message \n $message";
 
         mail($to,$email_subject,$email_body,$headers);
         //redirect to the 'thank you' page
